bb635c78bfeac6167f282461a1f04e942147b2ce
AlexandrRepkin <arepkin96@mail.ru>
2023-05-10 17:07:08 +0300

    ErrorBoundary

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -25,13 +25,17 @@ class RandomChar extends Component {
         this.setState({char, loading: false})
     }
 
+    onCharLoading = () => {
+        this.setState({loading: true})
+    }
+
     onError = () => {
         this.setState({loading: false, error: true})
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.onCharLoaded()
+        this.onCharLoading()
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -73,10 +77,10 @@ const View = (char) => {
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
-    
+
     return(
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img style={imgStyle} src={thumbnail} alt="Random character" className="randomchar__img"/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -26,11 +26,13 @@ _apiKey = 'apikey=4eacfbc1b939786ef3757505cb5c1785'
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'Данных про этого персонажа нет',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
